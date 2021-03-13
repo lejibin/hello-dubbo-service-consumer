c5ac04e90dbcb518ea462a078bd0c09064f70859
Richard <richard@cnheri.com>
2021-03-13 14:27:43 +0800

    Initial commit

---
+++ src/main/java/com/binhow/hello/dubbo/service/consumer/HelloDubboServiceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.binhow.hello.dubbo.service.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDubboServiceConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDubboServiceConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/binhow/hello/dubbo/service/consumer/HelloDubboServiceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binhow.hello.dubbo.service.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDubboServiceConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
